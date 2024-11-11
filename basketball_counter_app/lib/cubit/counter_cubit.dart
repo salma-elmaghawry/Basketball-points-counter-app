f23bd7616a3948200e273aa956d86edad3b6539e
salma <salmaelmaghawry91@gmail.com>
2024-11-11 14:10:36 +0200

    incremet funvtion in cubit

--- basketball_counter_app/lib/cubit/counter_cubit.dart
+++ basketball_counter_app/lib/cubit/counter_cubit.dart
@@ -1,6 +1,18 @@
 import 'package:basketball_counter_app/cubit/counter_state.dart';
 import 'package:bloc/bloc.dart';
 
-class CounterCubit extends Cubit<CounterState>{
-  CounterCubit():super(CounterBIncrement());
+class CounterCubit extends Cubit<CounterState> {
+  CounterCubit() : super(CounterBIncrement());
+
+  int teamAPoints = 0;
+  int teamBPoints = 0;
+  void teamIncrement(String team, int buttonNumber) {
+    if (team == "A") {
+      teamAPoints += buttonNumber;
+      emit(CounterAIncrement());
+    } else {
+      teamBPoints += buttonNumber;
+      emit(CounterBIncrement());
+    }
+  }
 }
